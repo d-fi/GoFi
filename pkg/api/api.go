a383e3adb95fba08ccd9f39113616258f21bd601
Sayem Chowdhury <sayem314@gmail.com>
2024-09-26 23:13:56 +0600

    add api

---
+++ pkg/api/api.go
@@ -0,0 +1,242 @@
+package api
+
+import (
+	"encoding/json"
+	"fmt"
+
+	"github.com/d-fi/GoFi/pkg/request"
+	"github.com/d-fi/GoFi/pkg/types"
+)
+
+func GetTrackInfoPublicApi(sngID string) (types.TrackTypePublicAPI, error) {
+	var result types.TrackTypePublicAPI
+	data, err := request.RequestPublicApi("/track/" + sngID)
+	if err != nil {
+		return result, err
+	}
+	err = json.Unmarshal(data, &result)
+	return result, err
+}
+
+func GetAlbumInfoPublicApi(albID string) (types.AlbumTypePublicApi, error) {
+	var result types.AlbumTypePublicApi
+	data, err := request.RequestPublicApi("/album/" + albID)
+	if err != nil {
+		return result, err
+	}
+	err = json.Unmarshal(data, &result)
+	return result, err
+}
+
+func GetTrackInfo(sngID string) (types.TrackType, error) {
+	var result types.TrackType
+	data, err := request.Request(map[string]interface{}{"sng_id": sngID}, "song.getData")
+	if err != nil {
+		return result, err
+	}
+	err = json.Unmarshal(data, &result)
+	return result, err
+}
+
+func GetLyrics(sngID string) (types.LyricsType, error) {
+	var result types.LyricsType
+	data, err := request.Request(map[string]interface{}{"sng_id": sngID}, "song.getLyrics")
+	if err != nil {
+		return result, err
+	}
+	err = json.Unmarshal(data, &result)
+	return result, err
+}
+
+func GetAlbumInfo(albID string) (types.AlbumType, error) {
+	var result types.AlbumType
+	data, err := request.Request(map[string]interface{}{"alb_id": albID}, "album.getData")
+	if err != nil {
+		return result, err
+	}
+	err = json.Unmarshal(data, &result)
+	return result, err
+}
+
+func GetAlbumTracks(albID string) (types.AlbumTracksType, error) {
+	var result types.AlbumTracksType
+	data, err := request.Request(map[string]interface{}{
+		"alb_id": albID,
+		"lang":   "us",
+		"nb":     -1,
+	}, "song.getListByAlbum")
+	if err != nil {
+		return result, err
+	}
+	err = json.Unmarshal(data, &result)
+	return result, err
+}
+
+func GetPlaylistInfo(playlistID string) (types.PlaylistInfo, error) {
+	var result types.PlaylistInfo
+	data, err := request.Request(map[string]interface{}{
+		"playlist_id": playlistID,
+		"lang":        "en",
+	}, "playlist.getData")
+	if err != nil {
+		return result, err
+	}
+	err = json.Unmarshal(data, &result)
+	return result, err
+}
+
+func GetPlaylistTracks(playlistID string) (types.PlaylistTracksType, error) {
+	var result types.PlaylistTracksType
+	data, err := request.Request(map[string]interface{}{
+		"playlist_id": playlistID,
+		"lang":        "en",
+		"nb":          -1,
+		"start":       0,
+		"tab":         0,
+		"tags":        true,
+		"header":      true,
+	}, "playlist.getSongs")
+	if err != nil {
+		return result, err
+	}
+
+	if err := json.Unmarshal(data, &result); err != nil {
+		return result, fmt.Errorf("failed to unmarshal playlist tracks data: %v", err)
+	}
+
+	// Update track position in the playlist
+	for index, track := range result.Data {
+		// Check if TrackPosition is a pointer and allocate a new int if necessary
+		if track.TRACK_POSITION == nil {
+			track.TRACK_POSITION = new(int)
+		}
+		*track.TRACK_POSITION = index + 1
+	}
+
+	return result, nil
+}
+
+func GetArtistInfo(artID string) (types.ArtistInfoType, error) {
+	var result types.ArtistInfoType
+	data, err := request.Request(map[string]interface{}{
+		"art_id":         artID,
+		"filter_role_id": []int{0},
+		"lang":           "en",
+		"tab":            0,
+		"nb":             -1,
+		"start":          0,
+	}, "artist.getData")
+	if err != nil {
+		return result, err
+	}
+	err = json.Unmarshal(data, &result)
+	return result, err
+}
+
+func GetDiscography(artID string, nb int) (types.DiscographyType, error) {
+	var result types.DiscographyType
+	data, err := request.Request(map[string]interface{}{
+		"art_id":         artID,
+		"filter_role_id": []int{0},
+		"lang":           "en",
+		"nb":             nb,
+		"nb_songs":       -1,
+		"start":          0,
+	}, "album.getDiscography")
+	if err != nil {
+		return result, err
+	}
+	err = json.Unmarshal(data, &result)
+	return result, err
+}
+
+func GetProfile(userID string) (types.ProfileType, error) {
+	var result types.ProfileType
+	data, err := request.Request(map[string]interface{}{
+		"user_id": userID,
+		"tab":     "loved",
+		"nb":      -1,
+	}, "mobile.pageUser")
+	if err != nil {
+		return result, err
+	}
+	err = json.Unmarshal(data, &result)
+	return result, err
+}
+
+func SearchAlternative(artist, song string, nb int) (types.SearchType, error) {
+	var result types.SearchType
+	data, err := request.Request(map[string]interface{}{
+		"query": fmt.Sprintf("artist:'%s' track:'%s'", artist, song),
+		"types": []string{"TRACK"},
+		"nb":    nb,
+	}, "mobile_suggest")
+	if err != nil {
+		return result, err
+	}
+	err = json.Unmarshal(data, &result)
+	return result, err
+}
+
+func SearchMusic(query string, nb int, searchTypes ...string) (types.SearchType, error) {
+	var result types.SearchType
+
+	// Set default types if none are provided
+	if len(searchTypes) == 0 {
+		searchTypes = []string{"TRACK"}
+	}
+
+	data, err := request.Request(map[string]interface{}{
+		"query":          query,
+		"start":          0,
+		"nb":             nb,
+		"types":          searchTypes,
+		"suggest":        true,
+		"artist_suggest": true,
+		"top_tracks":     true,
+	}, "mobile_suggest")
+
+	if err != nil {
+		return result, err
+	}
+
+	if err := json.Unmarshal(data, &result); err != nil {
+		return result, fmt.Errorf("failed to unmarshal search results: %v", err)
+	}
+
+	return result, nil
+}
+
+func GetUser() (types.UserType, error) {
+	var result types.UserType
+	data, err := request.RequestGet("user_getInfo", nil)
+	if err != nil {
+		return result, err
+	}
+	err = json.Unmarshal(data, &result)
+	return result, err
+}
+
+func GetChannelList() (types.ChannelSearchType, error) {
+	var result types.ChannelSearchType
+	data, err := request.Request(nil, "search_getChannels")
+	if err != nil {
+		return result, err
+	}
+	err = json.Unmarshal(data, &result)
+	return result, err
+}
+
+func GetShowInfo(showID string, nb, start int) (types.ShowType, error) {
+	var result types.ShowType
+	data, err := request.Request(map[string]interface{}{
+		"SHOW_ID": showID,
+		"NB":      nb,
+		"START":   start,
+	}, "mobile.pageShow")
+	if err != nil {
+		return result, err
+	}
+	err = json.Unmarshal(data, &result)
+	return result, err
+}
---
+++ pkg/api/api_test.go
@@ -0,0 +1,157 @@
+package api
+
+import (
+	"os"
+	"strconv"
+	"strings"
+	"testing"
+
+	"github.com/d-fi/GoFi/pkg/request"
+	"github.com/stretchr/testify/assert"
+)
+
+const (
+	SNG_ID = "3135556" // Harder, Better, Faster, Stronger by Daft Punk
+	ALB_ID = "302127"  // Discovery by Daft Punk
+)
+
+func init() {
+	// Initialize the Deezer API for all tests
+	arl := os.Getenv("DEEZER_ARL")
+	_, err := request.InitDeezerAPI(arl)
+	if err != nil {
+		panic("Failed to initialize Deezer API: " + err.Error())
+	}
+}
+
+func TestGetUser(t *testing.T) {
+	response, err := GetUser()
+	assert.NoError(t, err)
+	assert.NotEmpty(t, response.BlogName)
+	assert.NotEmpty(t, response.Email)
+	assert.NotEmpty(t, response.UserID)
+	assert.Equal(t, "user", response.Type)
+}
+
+func TestGetTrackInfo(t *testing.T) {
+	response, err := GetTrackInfo(SNG_ID)
+	assert.NoError(t, err)
+	assert.Equal(t, SNG_ID, response.SNG_ID)
+	assert.Equal(t, "GBDUW0000059", response.ISRC)
+	assert.Equal(t, "51afcde9f56a132096c0496cc95eb24b", response.MD5_ORIGIN)
+	assert.Equal(t, "song", response.Type)
+}
+
+func TestGetTrackInfoPublicApi(t *testing.T) {
+	response, err := GetTrackInfoPublicApi(SNG_ID)
+	assert.NoError(t, err)
+	assert.Equal(t, SNG_ID, strconv.Itoa(response.ID))
+	assert.Equal(t, "GBDUW0000059", response.ISRC)
+	assert.Equal(t, "track", response.Type)
+}
+
+func TestGetLyrics(t *testing.T) {
+	response, err := GetLyrics(SNG_ID)
+	assert.NoError(t, err)
+	assert.NotNil(t, response.LYRICS_ID)
+	assert.Equal(t, "2780622", *response.LYRICS_ID)
+	assert.Greater(t, len(response.LYRICS_TEXT), 0)
+}
+
+func TestGetAlbumInfo(t *testing.T) {
+	response, err := GetAlbumInfo(ALB_ID)
+	assert.NoError(t, err)
+	assert.Equal(t, ALB_ID, response.ALB_ID)
+	assert.Equal(t, "724384960650", response.UPC)
+	assert.Equal(t, "album", response.TYPE_INTERNAL)
+}
+
+func TestGetAlbumInfoPublicApi(t *testing.T) {
+	response, err := GetAlbumInfoPublicApi(ALB_ID)
+	assert.NoError(t, err)
+	assert.Equal(t, ALB_ID, strconv.Itoa(response.ID))
+	assert.Equal(t, "724384960650", response.UPC)
+	assert.Equal(t, "album", response.Type)
+}
+
+func TestGetAlbumTracks(t *testing.T) {
+	response, err := GetAlbumTracks(ALB_ID)
+	assert.NoError(t, err)
+	assert.Equal(t, 14, response.Count)
+	assert.Equal(t, len(response.Data), response.Count)
+}
+
+func TestGetPlaylistInfo(t *testing.T) {
+	PLAYLIST_ID := "4523119944"
+	response, err := GetPlaylistInfo(PLAYLIST_ID)
+	assert.NoError(t, err)
+	assert.Greater(t, response.NbSong, 0)
+	assert.Equal(t, "sayem314", response.ParentUsername)
+	assert.Equal(t, "playlist", response.TYPE_INTERNAL)
+}
+
+func TestGetPlaylistTracks(t *testing.T) {
+	PLAYLIST_ID := "4523119944"
+	response, err := GetPlaylistTracks(PLAYLIST_ID)
+	assert.NoError(t, err)
+	assert.Greater(t, response.Count, 0)
+	assert.Equal(t, len(response.Data), response.Count)
+}
+
+func TestGetArtistInfo(t *testing.T) {
+	ART_ID := "13"
+	response, err := GetArtistInfo(ART_ID)
+	assert.NoError(t, err)
+	assert.Equal(t, "Eminem", response.ART_NAME)
+	assert.Equal(t, "artist", response.TYPE_INTERNAL)
+}
+
+func TestGetDiscography(t *testing.T) {
+	ART_ID := "13"
+	response, err := GetDiscography(ART_ID, 10)
+	assert.NoError(t, err)
+	assert.Equal(t, 10, response.Count)
+	assert.Equal(t, len(response.Data), response.Count)
+}
+
+func TestGetProfile(t *testing.T) {
+	USER_ID := "2064440442"
+	response, err := GetProfile(USER_ID)
+	assert.NoError(t, err)
+	assert.Equal(t, "sayem314", response.USER.BLOG_NAME)
+	assert.Equal(t, "user", response.USER.TYPE_INTERNAL)
+}
+
+func TestSearchAlternative(t *testing.T) {
+	ARTIST := "Eminem"
+	TRACK := "The Real Slim Shady"
+	response, err := SearchAlternative(ARTIST, TRACK, 10)
+	assert.NoError(t, err)
+	assert.Equal(t, "artist:'eminem' track:'the real slim shady'", response.QUERY)
+	assert.Equal(t, len(response.TRACK.Data), response.TRACK.Count)
+}
+
+func TestSearchMusic(t *testing.T) {
+	QUERY := "Eminem"
+	response, err := SearchMusic(QUERY, 1, "TRACK", "ALBUM", "ARTIST")
+	assert.NoError(t, err)
+	assert.Equal(t, strings.ToLower(QUERY), response.QUERY)
+	assert.Greater(t, response.TRACK.Count, 0)
+	assert.Greater(t, response.ALBUM.Count, 0)
+	assert.Greater(t, response.ARTIST.Count, 0)
+}
+
+func TestGetChannelList(t *testing.T) {
+	response, err := GetChannelList()
+	assert.NoError(t, err)
+	assert.Greater(t, response.Count, 0)
+	assert.Equal(t, len(response.Data), response.Count)
+}
+
+func TestGetShowInfo(t *testing.T) {
+	response, err := GetShowInfo("338532", 10, 0)
+	assert.NoError(t, err)
+	assert.Equal(t, "201952", response.Data.LabelID)
+	assert.Equal(t, 10, response.Episodes.Count)
+	assert.True(t, len(response.Episodes.Data) > 0)
+}

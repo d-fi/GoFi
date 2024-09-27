fd7b820b1dcc3b7b119b76bcc1d0677b511f0607
Sayem Chowdhury <sayem314@gmail.com>
2024-09-28 00:52:26 +0600

    update url

--- utils/url.go
+++ utils/url.go
@@ -4,30 +4,38 @@ import (
 	"errors"
 	"fmt"
 	"net/http"
+	"time"
 
 	"github.com/d-fi/GoFi/logger"
 )
 
 // CheckURLFileSize performs a HEAD request to check the availability of a URL
 // and returns the content length if available.
-func CheckURLFileSize(url string) (int, error) {
+func CheckURLFileSize(url string) (int64, error) {
 	logger.Debug("Checking URL file size for: %s", url)
 
-	resp, err := http.Head(url)
+	client := &http.Client{
+		Timeout: 10 * time.Second,
+	}
+
+	resp, err := client.Head(url)
 	if err != nil {
 		logger.Debug("Error during HEAD request: %v", err)
 		return 0, err
 	}
 	defer resp.Body.Close()
 
-	contentLength := resp.Header.Get("Content-Length")
-	if contentLength != "" {
-		var size int
-		fmt.Sscanf(contentLength, "%d", &size)
-		logger.Debug("Determined file size: %d bytes", size)
-		return size, nil
+	if resp.StatusCode < 200 || resp.StatusCode >= 300 {
+		logger.Debug("Non-success status code: %d", resp.StatusCode)
+		return 0, fmt.Errorf("received non-success status code: %d", resp.StatusCode)
 	}
 
-	logger.Debug("Unable to determine file size for URL: %s", url)
-	return 0, errors.New("unable to determine file size")
+	contentLength := resp.ContentLength
+	if contentLength >= 0 {
+		logger.Debug("Determined file size: %d bytes", contentLength)
+		return contentLength, nil
+	}
+
+	logger.Debug("Content-Length header is missing")
+	return 0, errors.New("content-length header is missing")
 }

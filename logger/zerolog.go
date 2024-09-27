814f05374661653a116102fea0c3fde552f24c1f
Sayem Chowdhury <sayem314@gmail.com>
2024-09-27 20:43:53 +0600

    update logger

--- logger/zerolog.go
+++ logger/zerolog.go
@@ -2,30 +2,30 @@ package logger
 
 import (
 	"os"
+	"strings"
 
 	"github.com/rs/zerolog"
 	"github.com/rs/zerolog/log"
 )
 
-func Initialize(logLevel string) {
-	level := zerolog.DebugLevel
+func init() {
+	// Read the DEBUG environment variable to determine the log level
+	debugEnv := strings.ToLower(os.Getenv("DEBUG"))
+	level := zerolog.InfoLevel
 
-	switch logLevel {
-	case "debug":
+	if debugEnv == "true" {
 		level = zerolog.DebugLevel
-	case "info":
-		level = zerolog.InfoLevel
-	case "warn":
-		level = zerolog.WarnLevel
-	case "error":
-		level = zerolog.ErrorLevel
-	default:
-		log.Warn().Msgf("Invalid log level '%s', defaulting to Debug level", logLevel)
 	}
 
-	zerolog.SetGlobalLevel(level)
+	// Create a new logger with the desired level
+	logger := zerolog.New(zerolog.ConsoleWriter{
+		Out:        os.Stderr,
+		TimeFormat: "2006-01-02 15:04:05",
+		NoColor:    false,
+	}).Level(level).With().Timestamp().Logger()
 
-	log.Logger = log.Output(zerolog.ConsoleWriter{Out: os.Stderr})
+	// Assign the new logger to the package-level logger
+	log.Logger = logger
 }
 
 func Debug(msg string, args ...interface{}) {
